2c84206aacddc193c5f1ecf6467b33f748c20ef5
Kazuki Okamoto <kazuki.okamoto@kakkun61.com>
2019-11-15 13:32:09 +0900

    Psake tasks

---
+++ psakefile.ps1
@@ -0,0 +1,18 @@
+Task Import {
+    Import-Module powershell-yaml, Pester, PSScriptAnalyzer
+}
+
+Task Test {
+    Invoke-Pester
+}
+
+Task Lint {
+    Invoke-ScriptAnalyzer . -Recurse -Severity Information
+}
+
+Task Publish {
+    Publish-Module `
+      -Name ..\ghcups `
+      -NuGetApiKey (Get-Content .psg.key) `
+      -Exclude '.github', '.psg.key'
+}
