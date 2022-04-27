11fb21d4ed29bb8214cfd598125947fdb2666b22
Kazuki Okamoto <kazuki.okamoto@kakkun61.com>
2022-04-27 13:11:38 +0900

    a Version instance to a string instance

--- ghcman.psm1
+++ ghcman.psm1
@@ -4,7 +4,7 @@ Set-StrictMode -Version Latest
 
 # Constant
 
-Set-Variable ghcmanVersion -Option Constant -Value [Version]"4.8"
+Set-Variable ghcmanVersion -Option Constant -Value "4.8"
 Set-Variable systemGlobalDataPath -Option Constant -Value "$Env:ProgramData\ghcman"
 Set-Variable userGlobalDataPath -Option Constant -Value "$Env:APPDATA\ghcman"
 Set-Variable versionPattern -Option Constant -Value '[0-9]+(\.[0-9]+)*'
